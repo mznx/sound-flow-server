88b2d42b52ad0230ce1436d6b3df0e5f41d84170
mznx <mznx.ux@gmail.com>
2022-01-25 12:49:09 +0400

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import config  from './config';
+import routes  from './routes';
 
 const port = process.env.PORT || config.port;
 const app = express();
+
+routes(app);
 
 app.listen(port);
 console.log(`[Start]\n port: ${port}\n time: ${new Date().toISOString()}`);
---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+export default function (app) {
+  app.get('/login', /* callback */);
+  app.get('/callback', /* callback */);
+  app.get('/refresh_token', /* callback */);
+  app.get('/check_token', /* callback */);
+}

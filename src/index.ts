a8c83a5e52bce61951ee873d94d73d433c9d1582
mznx <mznx.ux@gmail.com>
2022-01-25 12:56:31 +0400

    add app.use to index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ import routes  from './routes';
 const port = process.env.PORT || config.port;
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('[Connect]');
+  console.log(' from: ', req.ip);
+  console.log(' path: ', req.path);
+  console.log(' time: ', new Date().toISOString());
+  next();
+});
+
 routes(app);
 
 app.listen(port);

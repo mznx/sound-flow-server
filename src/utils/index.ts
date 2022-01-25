fa45b151bbca7eefd4a246006383048be68c4676
mznx <mznx.ux@gmail.com>
2022-01-25 13:02:49 +0400

    add utils (generateRandomString)

---
+++ src/utils/index.ts
@@ -0,0 +1,18 @@
+const utils = {
+  /**
+   * Generates a random string containing numbers and letters
+   * @param  {number} length The length of the string
+   * @return {string} The generated string
+   */
+  generateRandomString: function (length: number) {
+    let text: string = '';
+    const possible: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+};
+
+export default utils;

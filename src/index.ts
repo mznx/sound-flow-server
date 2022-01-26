cd5741bb4790c162628baeedb9d6ff96d498a2c6
mznx <mznx.ux@gmail.com>
2022-01-26 17:24:50 +0400

    add cors dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors    from 'cors';
 import config  from './config';
 import routes  from './routes';
 
 const port = process.env.PORT || config.port;
 const app = express();
 
-app.use((req, res, next) => {
+app.use(cors()).use((req, res, next) => {
   console.log('[Connect]');
   console.log(' from: ', req.ip);
   console.log(' path: ', req.path);
